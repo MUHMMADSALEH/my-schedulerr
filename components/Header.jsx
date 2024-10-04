88924d18806e8a23b06131018e494d606f3b0150
MUHMMADSALEH <khanmohdsaleh2002@gmail.com>
2024-10-04 11:17:26 +0530

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,40 @@
+"use client"
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+import { PenBox } from 'lucide-react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import UserMenu from './user-menu'
+
+
+const Header = () => {
+  return (
+    <nav className='mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2'>
+      <Link href={"/"} className='flex items-center '>
+        <Image src="/logo.png" alt="logo" width="150" height="60" className='h-16 w-auto '
+        />
+      </Link>
+
+      <div className='flex items-center gap-4'>
+        <Link href="/events?create=true">
+          <Button className="flex items-center gap-2" ><PenBox size={18} />Create Event</Button>
+        </Link>
+        {/* it  will  only be   rendering  when user is not loggedin */}
+        <SignedOut>
+          <SignInButton forceRedirectUrl='/dashboard'>
+
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        {/* it  will   only be rendering  when user is signedin */}
+        <SignedIn>
+         <UserMenu/>
+        </SignedIn> 
+
+      </div>
+    </nav>
+  )
+}
+
+export default Header
