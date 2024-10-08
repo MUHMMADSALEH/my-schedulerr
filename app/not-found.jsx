6ae36efd6972941a05d25e98e2df9e51b13fe2d2
MUHMMADSALEH <khanmohdsaleh2002@gmail.com>
2024-10-08 19:47:22 +0530

    feat:add dashboard

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="text-3xl md:6xl font-extrabold flex justify-center mt-80 w-screen"> <h1>404-page Not Found</h1></div>
+  )
+}
+
+export default NotFound
